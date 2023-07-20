e3c96ee0e37b6ac9640fd8854ab79753970fe282
Misael Sena <misaellsena@gmail.com>
2023-07-19 22:02:34 -0300

    Conclusão do Exercício 2

--- index.js
+++ index.js
@@ -16,4 +16,28 @@ function exercicio1(){
     }  
   }
 
-  exercicio1();
+  //exercicio1();
+
+/*Exercício 2:  Escreva uma função em JavaScript que receba dois números
+ *  inteiros como parâmetros e retorne o maior número entre
+ *  eles.
+ */
+
+function exercicio2(){
+  var numero1 = prompt("Digite o primeiro número: ");  
+  var numero2 = prompt("Digite o segundo número: ");
+
+      if(numero1 == Math.floor(numero1) && numero2 == Math.floor(numero2)){
+          if(numero1>numero2){
+            console.log(`O número ${numero1} é maior que o número ${numero2}!`);
+          }else if(numero1<numero2){
+            console.log(`O número ${numero2} é maior que o número ${numero1}!`);
+          }else{
+            console.log(`Os números digitados são iguais!`);
+          }
+      }else{
+          console.log("Os números digitados devem ser inteiros!");
+          return exercicio2();
+      }
+}
+exercicio2();
