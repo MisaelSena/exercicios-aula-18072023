aaa676366f1d9abd115a985475c001e80d469b24
Misael Sena <misaellsena@gmail.com>
2023-07-19 23:39:16 -0300

    Conclusão do Exercício 3

--- index.js
+++ index.js
@@ -18,26 +18,62 @@ function exercicio1(){
 
   //exercicio1();
 
-/*Exercício 2:  Escreva uma função em JavaScript que receba dois números
+/* Exercício 2:  Escreva uma função em JavaScript que receba dois números
  *  inteiros como parâmetros e retorne o maior número entre
  *  eles.
  */
 
 function exercicio2(){
-  var numero1 = prompt("Digite o primeiro número: ");  
-  var numero2 = prompt("Digite o segundo número: ");
+  var numero1 = prompt("Digite o primeiro número inteiro: ");  
+  var numero2 = prompt("Digite o segundo número inteiro: ");
 
       if(numero1 == Math.floor(numero1) && numero2 == Math.floor(numero2)){
-          if(numero1>numero2){
-            console.log(`O número ${numero1} é maior que o número ${numero2}!`);
-          }else if(numero1<numero2){
-            console.log(`O número ${numero2} é maior que o número ${numero1}!`);
-          }else{
-            console.log(`Os números digitados são iguais!`);
-          }
-      }else{
-          console.log("Os números digitados devem ser inteiros!");
-          return exercicio2();
-      }
+        if(numero1>numero2){
+          console.log(`O número ${numero1} é maior que o número ${numero2}!`);
+        }else if(numero1<numero2){
+          console.log(`O número ${numero2} é maior que o número ${numero1}!`);
+        }else{
+          console.log(`Os números digitados são iguais!`);
+        }
+    }else{
+        console.log("Os números digitados devem ser inteiros!");
+        return exercicio2();
+    }
 }
-exercicio2();
+//exercicio2();
+
+  /* Exercício 3: Escreva um programa em JavaScript que solicite ao
+   * usuário um número inteiro positivo e exiba na tela todos os números
+   * primos até esse número. 
+   */
+
+function exercicio3(){
+  var numero = prompt("Digite um número inteiro positivo: ");
+
+      if(numero === 0){
+        console.log("Você digitou o número 0! Por Favor digite outro!");
+        return exercicio3();
+      }else if(numero<0){
+        console.log(`Você digitou o número ${numero} que é negativo! Ditite um número maior que zero!`);
+        return exercicio3();
+      }else{
+        /*Um número primo é um número inteiro maior que 1 que possui 
+         * apenas dois divisores positivos: o número 1 e ele mesmo.
+         */
+        for(i = 2; i<=numero; i++){
+          var numeroPrimo = true;
+
+          for(divisor = 2; divisor < i; divisor++){
+             if(i % divisor === 0 ){
+              numeroPrimo = false;              
+             } 
+          }
+        
+            if(numeroPrimo){
+              console.log(i);
+            }
+          }
+      }
+    }
+    
+exercicio3();
